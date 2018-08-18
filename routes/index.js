147a2e13c2c2d7ed97011beb621a13e132f61d04
Nick <nicholasjohnrichardson@gmail.com>
2018-08-18 18:09:30 +0100

    more linting

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const ApiCall = require('../public/javascripts/apicall.js')
+const ApiCall = require('../public/javascripts/apicall.js');
 const apicall = new ApiCall();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FT Search', articles: [] });
+  res.render('index', {
+    articles: [],
+    title: 'FT Search'
+  });
 });
 
 router.post('/', function(req, res, next) {
   apicall.search(req.body.keyword).then((articles) => {
-    res.render('index', { title: 'FT Search', articles: articles });
-  })
-})
+    res.render('index', {
+      articles: articles,
+      title: 'FT Search'
+    });
+  });
+});
 
 module.exports = router;
