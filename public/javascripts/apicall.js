4da4f4679c1ca11dfa2ca56b4bbd839db493fb26
Nick <nicholasjohnrichardson@gmail.com>
2018-08-18 18:05:58 +0100

    corrected some linting issues

--- public/javascripts/apicall.js
+++ public/javascripts/apicall.js
@@ -1,6 +1,7 @@
-const request = require("request");
-const datahandler = require("./datahandler.js")
-require('dotenv').config()
+const request = require('request');
+const datahandler = require('./datahandler.js');
+
+require('dotenv').config();
 
 module.exports = class ApiCall {
   constructor() {
@@ -9,28 +10,31 @@ module.exports = class ApiCall {
 
   search(keyword) {
   return new Promise((resolve, reject) => {
-    let options = {
-      method: 'POST',
-      url: 'http://api.ft.com/content/search/v1',
-      json: true,
+    const options = {
+      body: { 'queryString': keyword,
+      'resultContext': { 'aspects': [
+          'title',
+          'lifecycle',
+          'location',
+          'summary',
+          'editorial'
+        ] } },
       headers: { 'X-Api-Key': this._apikey },
-      body: {
-      "queryString": keyword,
-      "resultContext": {
-        "aspects" :[ "title", "lifecycle", "location", "summary", "editorial"]}
-      }
+      json: true,
+      method: 'POST',
+      url: 'http://api.ft.com/content/search/v1'
     };
-
-    let articles = [];
+    const articles = [];
 
     request(options, function (error, response, body) {
-      if (error) {reject(new Error(error))};
+      if (error) {
+        reject(new Error(error));
+      }
       body.results[0].results.forEach((article) => {
-        articles.push(datahandler.processArticle(article))
-      })
+        articles.push(datahandler.processArticle(article));
+      });
       resolve(articles);
       });
     });
   }
-
-}
+};
--- public/javascripts/datahandler.js
+++ public/javascripts/datahandler.js
@@ -1,7 +1,9 @@
-module.exports.processArticle = ((article) => {
-  let item = {};
+module.exports.processArticle = (article) => {
+  const item = {};
+
   item.title = article.title.title;
   item.url = article.location.uri;
-  item.date = article.lifecycle.initialPublishDateTime.slice(0,10);
+  item.date = article.lifecycle.initialPublishDateTime.slice(0, 10);
+
   return item;
-})
+};
